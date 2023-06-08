b8e4a382eeb8a719d4db00e696aa45781016b5b8
mateusvmv <mateusv251@gmail.com>
2023-06-08 12:21:33 -0300

    Atualiza documentação

--- include/tilemap.hpp
+++ include/tilemap.hpp
@@ -8,40 +8,62 @@
 #include <tmxlite/TileLayer.hpp>
 #include "types.hpp"
 
+/// Guarda um tileset e as texturas utilizadas por ele
 class TileSet {
 	private:
 	std::vector<sf::Texture> textures;
 	u32 cellSize;
 
 	public:
+	/// Tipo interior
 	const tmx::Tileset& inner;
+	/// Cria um novo TileSet a partir de um Tileset do tmxlite
 	TileSet(const tmx::Tileset& inner);
+	/// Retorna a textura associada ao tile com o id especificado
 	const sf::Texture& getTexture(u32 i) const;
+	/// Retorna os quatro vértices que formam a textura desse tile, na posição especificada
 	std::array<sf::Vertex, 4> getQuad(sf::Vector2f pos, u32 index, u32 flips) const;
 };
 
+/// Guarda dados relevantes à renderização de tiles estáticos
 struct TileRender {
+	/// O Tileset que contém este tile
 	const TileSet* tileset;
+	/// A origem, no ponto superior esquerdo do tile
 	sf::Vector2f position;
+	/// Flags de rotação. Veja [FlipFlag](https://codedocs.xyz/fallahn/tmxlite/classtmx_1_1TileLayer.html#a64883c49573c7766a86a9823b5b8f31b)
 	u8 flipFlags;
+	/// O ID global deste tile, que o identifica dentre todos os tilesets
 	u32 GID;
+	/// Guarda as informações para renderizar um tile
 	TileRender(const TileSet* tileset, sf::Vector2u position, u8 flipFlags, u32 GID);
+	/// Coloca vertices em um buffer para serem renderizados
 	void prepare() const;
 };
 
+/// Guarda dados relevantes à renderização de tiles animados
 struct TileAnimation: TileRender {
+	/// A lista de frames contidos nesta animação
 	std::vector<tmx::Tileset::Tile::Animation::Frame> frames;
+	/// O índice do frame atual
 	u32 index;
+	/// O tempo gasto no frame atual
 	f64 elapsed;
+	/// Guarda as informações para renderizar um tile animado
 	TileAnimation(TileRender render, tmx::Tileset::Tile::Animation animation);
 };
 
+/// Guarda dados relevantes à colisão de tiles
 struct TileCollision {
+	/// A origem deste tile
 	sf::Vector2u origin;
+	/// As caixas de colisão contidas nesse tile
 	const std::vector<tmx::Object>& objects;
+	/// Guarda as informações para registrar a colisão de um tile
 	TileCollision(sf::Vector2u origin, const std::vector<tmx::Object>& objects);
 };
 
+/// Guarda uma camada de tiles e as texturas, animações e colisões associadas
 class TileLayer: public sf::Drawable {
 	private:
 	const std::map<u32, TileSet>& tilesets;
@@ -52,11 +74,15 @@ class TileLayer: public sf::Drawable {
 	void draw(sf::RenderTarget& rt, sf::RenderStates states) const;
 
 	public:
+	/// O tipo interior
 	const tmx::TileLayer& inner;
+	/// Atualiza animações
 	void update(sf::Time time);
+	/// Cria uma nova layer de tiles a partir de um tipo interior e dos tilesets associados a um mapa
 	TileLayer(const tmx::TileLayer& layer, const tmx::Map& map, const std::map<u32, TileSet>& tilesets);
 };
 
+/// Guarda um mapa de tiles e as camadas associadas
 class TileMap: public sf::Drawable {
 	friend class TileLayer;
 	private:
@@ -66,7 +92,9 @@ class TileMap: public sf::Drawable {
 	void draw(sf::RenderTarget& rt, sf::RenderStates states) const;
 
 	public:
+	/// Atualiza animações
 	void update(sf::Time time);
+	/// Cria um novo tilemap a partir do tipo interior do tmxlite
 	TileMap(tmx::Map&& map);
 };
 
