c01cdf8a16a4534d4826c8d19410f3092bc5a7ac
IagoNath <128027348+IagoNath@users.noreply.github.com>
2023-07-03 20:54:41 -0300

    Update player.hpp

--- include/player.hpp
+++ include/player.hpp
@@ -4,6 +4,7 @@
 #include "collidable.hpp"
 #include "weapon.hpp"
 #include "assets.hpp"
+#include "render.hpp"
 
 struct PlayerProperties {
     double _health;
@@ -28,9 +29,8 @@ private:
 public:
     PlayerGUI(const Player& player);
 
-    std::vector<sf::Drawable*> get_gui_drawables();
-
     void updateHPBar();
+    void renderHPBar(ZRenderer& renderer);
 
     ~PlayerGUI() = default;
 };
@@ -41,12 +41,12 @@ private:
     static constexpr const u32 _categoryBits = 0|PLAYER|DYNAMIC;
     static constexpr const u32 _maskBits = 0|STATIC|PLAYER|ENEMY;
 
+    b2Vec2 velocity;
     PlayerGUI _gui;
 
     void default_config(WeaponType& weaponType);
     
 public:
-    b2Vec2 velocity;
 
     Player(float x, float y, b2World* world, Shapeb2* shape, std::string texture, PlayerProperties &&pProperties, WeaponType weaponType);
     Player(float x, float y, b2World* world, Shapeb2* shape, sf::Color color, PlayerProperties &&pProperties, WeaponType weaponType);
@@ -54,14 +54,15 @@ public:
     Weapon* get_weapon() {return _weapon;}
     PlayerProperties& get_properties() {return _pProperties;}
     const PlayerProperties& get_properties() const {return _pProperties;}
-    std::vector<sf::Drawable*> get_drawables() {return _drawables;}
+    const b2Vec2& get_velocity() const {return velocity;}
 
     void _move(sf::RenderWindow &window, sf::View& camera);
-    void _attack();
-    
+    void _attack(sf::RenderWindow &window);
+    void handleAttack(sf::RenderWindow &window);
+    PlayerGUI& getGUI() {return _gui;}
+
     ~Player();
 protected:
     Weapon* _weapon;
     PlayerProperties _pProperties;
-    std::vector<sf::Drawable*> _drawables;
 };
